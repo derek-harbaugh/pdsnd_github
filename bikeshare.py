8e56f2bc74d477e918f71eebeedfea5e2d142e36
Derek-Forbes-Harbaugh <derek.harbaugh@symetra.com>
2020-02-24 19:25:13 -0800

    added important closing message when user finishes using program

--- bikeshare.py
+++ bikeshare.py
@@ -232,6 +232,7 @@ def main():
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
+            print("YOU'RE DONE!!!!"")
             break
 
 
